62acb6bfc70976398f3c5ab10f2d71a931679b34
tiffany-meng <tmeng@ucsd.edu>
2021-04-19 16:10:16 -0700

    twitter

--- Twitter/TwitterAPICaller.swift
+++ Twitter/TwitterAPICaller.swift
@@ -10,7 +10,7 @@ import UIKit
 import BDBOAuth1Manager
 
 class TwitterAPICaller: BDBOAuth1SessionManager {    
-    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "uFTmFW66AAMEUwx3rZlZDMSCf", consumerSecret: "LtlxIoQpBvHcqjpSMIA9Gs2E9wCJbr7xkx9EpSdBYoNedaZUgh")
+    static let client = TwitterAPICaller(baseURL: URL(string: "https://api.twitter.com"), consumerKey: "mVPoHDIQXgdwE8eLHWu2TQUgl", consumerSecret: "b2Dg3LzMqrEXqGvsQrazJUrnSQl39ggunrbPAmgOIG4I9Xop95")
     var loginSuccess: (() -> ())?
     var loginFailure: ((Error) -> ())?
     
